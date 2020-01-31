31f441624b3002ea6032bb5b0886925108c44ff5
“Marko <marko.leht@khk.ee>
2020-01-31 17:10:51 +0200

    Update - app - views - users - register.php

--- app/views/users/register.php
+++ app/views/users/register.php
@@ -1,4 +1,6 @@
-<?php require_once APPROOT.'/views/inc/header.php'; ?>
+<?php
+require_once APPROOT.'/views/inc/header.php';
+?>
     <div class="row">
         <div class="col-md-6 mx-auto">
             <!-- form card register -->
@@ -8,22 +10,26 @@
                     <p class="mt-2">Please fill the fields below to register</p>
                 </div>
                 <div class="card-body">
-                    <form class="form" role="form">
+                    <form class="form" role="form" method="post" action="<?php echo URLROOT.'/users/register';?>">
                         <div class="form-group">
                             <label for="name">Name<sup>*</sup></label>
-                            <input type="text" class="form-control" id="name" placeholder="Full name" name = "name">
+                            <input type="text" class="form-control <?php echo (!empty($data['name_err'])) ? 'is-invalid' : ''; ?>" id="name" placeholder="Full name" name="name" value="<?php echo (!empty($data['name'])) ? $data['name'] : '';?>">
+                            <?php echo (!empty($data['name_err'])) ? '<span class="invalid-feedback">'.$data['name_err'].'</span>' : '';?>
                         </div>
                         <div class="form-group">
                             <label for="email">Email<sup>*</sup></label>
-                            <input type="email" class="form-control" id="email" placeholder="Email" name = email>
+                            <input type="text" class="form-control <?php echo (!empty($data['email_err'])) ? 'is-invalid' : ''; ?>" id="email" placeholder="Email" name="email" value="<?php echo (!empty($data['email'])) ? $data['email'] : '';?>">
+                            <?php echo (!empty($data['email_err'])) ? '<span class="invalid-feedback">'.$data['email_err'].'</span>' : '';?>
                         </div>
                         <div class="form-group">
                             <label for="password">Password<sup>*</sup></label>
-                            <input type="password" class="form-control" id="password" placeholder="Password">
+                            <input type="password" class="form-control <?php echo (!empty($data['password_err'])) ? 'is-invalid' : ''; ?>" id="password" placeholder="Password" name="password" value="<?php echo (!empty($data['password'])) ? $data['password'] : '';?>">
+                            <?php echo (!empty($data['password_err'])) ? '<span class="invalid-feedback">'.$data['password_err'].'</span>' : '';?>
                         </div>
                         <div class="form-group">
                             <label for="confirm_password">Verify password<sup>*</sup></label>
-                            <input type="password" class="form-control" id="confirm_password" placeholder="Confirm Password">
+                            <input type="password" class="form-control <?php echo (!empty($data['confirm_password_err'])) ? 'is-invalid' : ''; ?>" id="confirm_password" placeholder="Confirm Password" name="confirm_password" value="<?php echo (!empty($data['confirm_password'])) ? $data['confirm_password'] : '';?>">
+                            <?php echo (!empty($data['confirm_password_err'])) ? '<span class="invalid-feedback">'.$data['confirm_password_err'].'</span>' : '';?>
                         </div>
 
                         <div class="form-group">
@@ -36,4 +42,6 @@
 
         </div>
     </div>
-<?php require_once APPROOT.'/views/inc/footer.php'; ?>
+<?php
+require_once APPROOT.'/views/inc/footer.php';
+?>
