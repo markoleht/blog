e1579a06150ddcf4679a4e5fd748cc69ec6f7868
“Marko <marko.leht@khk.ee>
2020-02-03 09:36:44 +0200

    kasutaja email olemasolu kontroll andmebaasis - kui sellise emailiga rida on olemas, siis antud email on juba kasututsel ja seda ei tohi lubada registreerimisele, kui aga mitte - tulemus peab koosnema 0 reast ja siis saab lisada andmed andmebaasi

--- app/models/User.php
+++ app/models/User.php
@@ -3,14 +3,30 @@
 
 class User
 {
-  private $db;
+    private $db;
 
-  /**
-   * User constructor.
-   */
-  public function __construct()
-  {
-    $this->db = new Database();
-  }
+    /**
+     * User constructor.
+     */
+    public function __construct()
+    {
+        $this->db = new Database();
+    }
 
+
+    public function findUserByEmail($email)
+    {
+        $this->db->query('SELECT * FROM users WHERE email = :email');
+        $this->db->bind(':email', $email);
+        $row = $this->db->getOne();
+
+        if ($this->db->rowCount() > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
+
+
+
